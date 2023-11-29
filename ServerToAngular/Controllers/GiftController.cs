a5bc0a1006152ac06ce97454674810f6603cb656
214360810 <e0556686654@gmail.com>
2023-11-29 20:46:45 +0200

    Add project files.

---
+++ ServerToAngular/Controllers/GiftController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using ServerToAngular.Models;
+using System.Security.Cryptography.Xml;
+using System.Threading.Tasks;
+namespace ServerToAngular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class GiftController : ControllerBase
+    {
+
+
+        [Route("GetGifts")]
+        [HttpGet]
+        public List<Gift> GetGifts()
+        {
+            return Data.gifts;
+        }
+
+        [Route("AddGift")]
+        [HttpPost]
+        public int AddGift([FromBody] Gift gift)
+        {
+            if (gift.Id <= 0)
+            {
+                var max = Data.gifts.Max(x => x.Id);
+                gift.Id = max + 1;
+            }
+            Data.gifts.Add(gift);
+            return gift.Id;
+        }
+        [Route("UpdateGift")]
+        [HttpPut]
+        public bool UpdateGift([FromBody] Gift gift)
+        {
+            var g = Data.gifts.FirstOrDefault(x => x.Id == gift.Id);
+            if (g != null)
+            {
+                g.Name = gift.Name;
+                g.Donor = gift.Donor;
+                g.Cost = gift.Cost;
+                g.Image = gift.Image;
+                g.Description = gift.Description;
+                return true;
+            }
+            return false;
+        }
+        [Route("DeleteGift/{id}")]
+        [HttpDelete]
+        public bool DeleteGift( [FromQuery] int id)
+        {
+            var g = Data.gifts.FirstOrDefault(x => x.Id == id);
+            if (g != null)
+            {
+                Data.gifts.Remove(g);
+                return true;
+            }
+            return false;
+        }
+        [Route("Get/{id}")]
+        [HttpGet]
+        public Gift GetById(int id)
+        {
+            return Data.gifts.FirstOrDefault(x => x.Id == id);
+        }
+
+        ///donor:
+
+    }
+}
+
---
+++ ServerToAngular/Data.cs
@@ -0,0 +1,22 @@
+﻿using ServerToAngular.Models;
+
+namespace ServerToAngular
+{
+    public class Data
+    {
+        public static List<Donor> donors = new List<Donor>
+        {
+            new Donor(1,"aaa","aaaaaaaaaa") ,
+            new Donor(2,"bbb","bbbbbbbbbb"),
+            new Donor(3,"ccc","cccccccccc"),
+            new Donor(4,"ddd","dddddddddd")
+        };
+        public static List<Gift> gifts = new List<Gift>
+        {
+            new Gift(1,"camera",donors[0],30,"1.png","biggg") ,
+            new Gift(2,"car",donors[1],40,"2.png","fastttt"),
+            new Gift(3,"house",donors[2],50,"3.png","gggg"),
+            new Gift(4,"gift",donors[3],60,"4.png","jgdkj")
+        };
+    }
+}
